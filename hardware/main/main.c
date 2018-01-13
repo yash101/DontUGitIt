7973df629f3673f3ceafe2db4454e90f602cc88b
Dev Lodha <lodhad@gmail.com>
2018-01-13 11:44:12 -0700

    woops commiting in the wrong branch

---
+++ hardware/main/main.c
@@ -0,0 +1,29 @@
+#define GPS_MISO 16
+#define GPS_MOSI 17
+
+#define BUF_SIZE 1024
+
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_system.h"
+#include "driver/uart.h"
+#include "freertos/queue.h"
+#include "soc/uart_struct.h"
+
+static uart_config_t gps_config = {
+	.baud_rate = 9600,
+	.data_bits = UART_DATA_8_BITS,
+	.parity = UART_PARITY_DISABLE,
+	.stop_bits = UART_STOP_BITS_1,
+	.rx_flow_ctrl_thresh = 122
+};
+
+void app_main()
+{
+	uart_param_config(UART_NUM_2, &gps_config);
+	uart_set_pin(UART_NUM_2, GPS_MOSI, GPS_MISO);
+}
+
